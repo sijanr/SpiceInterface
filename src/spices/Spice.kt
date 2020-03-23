22c0ba71f85eb690c9428221b58841777e10f7e7
sijanr <47872289+sijanr@users.noreply.github.com>
2020-03-23 12:57:29 -0700

    Implement SpiceColor interface for Spices

--- src/spices/Spice.kt
+++ src/spices/Spice.kt
@@ -4,13 +4,15 @@ fun main(){
     val curry: Curry = Curry("non-spicy")
     curry.prepareSpice()
     curry.spiceDetails()
+    println("The color of the curry is: ${curry.spiceColor.color}")
 }
 
 //an abstract class Spice
-private abstract class Spice {
+private abstract class Spice: SpiceColor {
     abstract val spiceName: String
     abstract val spiciness: String
     abstract val heatLevel: Int
+    abstract val spiceColor: SpiceColor
 
     abstract fun spiceDetails()
     abstract fun prepareSpice()
@@ -19,7 +21,8 @@ private abstract class Spice {
 
 //Curry class that implements Spice as it is one of the spices and Grinder
 //as it needs to be grounded to turn it into a powder
-private class Curry(override val spiciness: String): Spice(), Grinder by Grind {
+private class Curry(override val spiciness: String, override val spiceColor: SpiceColor = YellowSpiceColor): Spice(), Grinder by Grind,
+                    SpiceColor by spiceColor{
 
     override val spiceName: String
         get() = "curry"
@@ -57,4 +60,14 @@ private object Grind: Grinder {
     override fun grind(objectName: String) {
         println("Grinding $objectName...")
     }
+}
+
+//interface for color of Spices
+private interface SpiceColor {
+    val color: String
+}
+
+//singleton subclass for YellowColor of spices
+object YellowSpiceColor: SpiceColor {
+    override val color = "YellowSpiceColor"
 }
